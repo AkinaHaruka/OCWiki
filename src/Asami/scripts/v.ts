b2eb26a76534af6467ff9f102ef732feb3d4fd0d
AkinaHaruka <misaka@mis1042.com>
2026-04-23 02:53:48 +0800

    opt:变量引用

---
+++ src/Asami/scripts/v.ts
@@ -0,0 +1,11 @@
+export const v = {
+    asa: "绘理",
+    sak: "朔夜",
+    mai: "舞萌",
+    qsc: "千岁川理工大学",
+    l: {
+        asa: '<a href="/Asami/designs/characters/浅见绘理">浅见绘理</a>',
+        sak: '<a href="/Asami/designs/characters/月城朔夜">月城朔夜</a>',
+        qsc: '<a href="/Asami/designs/world/千岁川理工大学">千岁川理工大学</a>'
+    }
+}
